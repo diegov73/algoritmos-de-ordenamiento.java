0bd031dc37ea1e3f3cafa9bbfa04702de53a1ddb
diegov73 <diego.valdes5@mail.udp.cl>
2025-03-21 00:06:40 -0300

    arreglo en clase propia'

--- ary.java
+++ ary.java
@@ -1,23 +1,28 @@
 import java.io.*;
 import java.util.Random;
 
-public class ary {
-    public static void main(String [] args) throws IOException{
-        
-        BufferedWriter BR = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader BW = new BufferedReader(new InputStreamReader(System.in));
-        Random srand = new Random();
-        boolean T = true;      
-        int C = 40;
-        int[] B = new int[C];
+class arreglo{
+    int C = 40;
+    int[] B = new int[C];
+    Random srand = new Random();
 
+    void arrre(){
         for(int i = 0; i<C; i++){
             int x = srand.nextInt(100);
             B[i] = x;
             System.out.println(B[i]);
         }
-        while(T!=true){
-            
-        }
+    }
+}
+
+public class ary {
+    public static void main(String [] args) throws IOException{
+        
+        BufferedWriter BR = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader BW = new BufferedReader(new InputStreamReader(System.in));
+        arreglo base = new arreglo();
+        boolean T = true;      
+
+        base.arrre();
     }
 }
